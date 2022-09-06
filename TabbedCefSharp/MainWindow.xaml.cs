16183d45b8b02405dbf02e911f865cc058511c61
SLT World <sltworld.py@gmail.com>
2022-09-06 15:18:57 +0800

    Upload

---
+++ TabbedCefSharp/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using CefSharp.Wpf.HwndHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TabbedCefSharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            NewTab();
+            NewTab();
+            NewTab();
+            NewTab();
+        }
+
+        private void NewTabButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewTab();
+        }
+
+        public void NewTab()
+        {
+            ChromiumWebBrowser Browser = new ChromiumWebBrowser("https://example.com");
+            Browser.Address = "https://example.com";
+            Tabs.Items.Add(new TabItem { Header = "Tab", Content = Browser });
+        }
+
+        private void CloseTabButton_Click(object sender, RoutedEventArgs e)
+        {
+            TabItem Tab = (TabItem)Tabs.Items[Tabs.SelectedIndex];
+            ChromiumWebBrowser _Browser = (ChromiumWebBrowser)Tab.Content;
+            Tab.Content = null;
+            _Browser.Dispose();
+            Tabs.Items.Remove(Tab);
+        }
+    }
+}
